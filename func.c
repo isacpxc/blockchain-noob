489aa13320b72cdaa3d55d69af10623e515a7e2d
isacpxc <isacpxc@gmail.com>
2024-11-07 18:15:54 -0300

    deleted

--- func.c
+++
@@ -1,6 +0,0 @@
-#include "func.h"
-
-const char* test(int i){
-    printf("print says: %d\n",i);
-    return "tudo ok";
-}
--- func.h
+++
@@ -1,6 +0,0 @@
-#ifndef _FUNC_H
-#define _FUNC_H
-
-const char* test(int);
-
-#endif // _FUNC_H
