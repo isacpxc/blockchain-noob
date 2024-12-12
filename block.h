7fca77a9f526673cfcb38e5fd7dcfc72e9aaf4c6
isacpxc <isacpxc@gmail.com>
2024-12-12 01:02:14 -0300

    feat: new structs added

--- block.h
+++ block.h
@@ -4,13 +4,16 @@
 #include <time.h>
 
 typedef struct {
+    time_t timestamp;
     char sender_wallet[13];
     char recipient_wallet[13];
     int amount;
+    int tax;
 } Transaction;
 
 typedef struct Address{
     char address[13];
+    int balance;
     struct Address *next;
 } Address;
 
@@ -32,5 +35,24 @@ typedef struct Block {
     Transaction trans;
     int data;
 } Block;
+
+typedef struct Message {
+    char type; // 'B' para Bloco, 'T' para Transação
+    union {
+        Block block;
+        Transaction transaction;
+    } data;
+    struct Message *last_message;
+} Message;
+
+typedef struct Node {
+    int id;  // identificador do nó
+    Block *blockchain;
+    Transaction transaction_pool[100]; // Pool de transações
+    Message *message_queue;
+    int transaction_count; // Contagem de transações
+    struct Node *next;
+    struct Node *prev;
+} Node;
 
 #endif // BLOCK_H
